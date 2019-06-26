73716323cd96f53f1efb9587d792c174661515ad
Isabelle <nema_i@hotmail.com>
2019-06-26 00:13:34 -0400

    minor changes to get rid of warnings

--- src/components/Completed.js
+++ src/components/Completed.js
@@ -4,14 +4,14 @@ import {connect} from 'react-redux';
 class Completed extends Component {
     getcompletedList = () => {return this.props.completed.length ? (this.props.completed.map((done, index) => {
       return (
-        <div className="completed collection" key={done.index}>
-          <div class="collection-item grey lighten-4">{done.content}</div>
+        <div className="completed collection" key={index}>
+          <div className="collection-item grey lighten-4">{done.content}</div>
         </div>
       );
     })) : (
       <p>You have not completeed any todo's!</p>
     )}
-    
+
     render () {return this.props.showCompleted ? (
       <div className="completed-list">
         {this.getcompletedList()}
