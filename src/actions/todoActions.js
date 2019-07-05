be6845f8341dd0cd66620e7fcd5cb2ad99db4520
Isabelle <nema_i@hotmail.com>
2019-07-05 19:40:49 -0400

    cleaned up todoActions.js

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,26 +1,14 @@
 export const deleteTodo = index => {
-  return {
-    type: 'DELETE_TODO',
-    index
-  }
+  return {type: 'DELETE_TODO', index}
 }
 
 export const completeTodo = index => {
-  return {
-    type: 'COMPLETE_TODO',
-    index
-  }
+  return {type: 'COMPLETE_TODO',index}
 }
 
 export const addTodo = todo => {
-  return {
-    type: 'ADD_TODO',
-    todo
-  }
+  return {type: 'ADD_TODO', todo}
 }
 export const setShowCompleted = showCompleted => {
-  return {
-    type: 'SET_SHOW_COMPLETED',
-    showCompleted
-  }
+  return {type: 'SET_SHOW_COMPLETED', showCompleted}
 }
